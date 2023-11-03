451d052bac468c288292d54a79738b134518183c
Kasmira Wijayathunga <mahidhawijayathunga@gmail.com>
2023-11-04 03:48:29 +0530

    FIX: moved window type definition to index.js

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+declare global {
+  interface Window {
+    Neutralino:any;
+  }
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
